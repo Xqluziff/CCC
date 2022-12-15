91615d8d6d11aeb394ec94005ce5b5f7a259cbaa
Xqluziff <xqluziff@yandex.ru>
2022-12-16 00:44:14 +0300

    Изменения

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -6,38 +6,38 @@
 */
 
 System.Console.WriteLine("Введите число");
-string x = Console.ReadLine() ;
+int x = Convert.ToInt32(System.Console.ReadLine());
 
 
-    if(x == "1")
-    {
-        System.Console.WriteLine("Пн");
-    }
-    else if(x == "2")
-    {
-        System.Console.WriteLine("Вт");
-    }
-    else if(x == "3")
-    {
-        System.Console.WriteLine("Ср");
-    }
-    else if(x == "4")
-    {
-        System.Console.WriteLine("Чт");
-    }
-    else if(x == "5")
-    {
-        System.Console.WriteLine("Пт");
-    }
-    else if(x == "6")
-    {
-        System.Console.WriteLine("Сб");
-    }
-    else if(x == "5")
-    {
-        System.Console.WriteLine("Вс");
-    }
-    else 
-    {
-        System.Console.WriteLine("Не корректное значения");
-    }
+if (x == 1)
+{
+    System.Console.WriteLine("Пн");
+}
+else if (x == 2)
+{
+    System.Console.WriteLine("Вт");
+}
+else if (x == 3)
+{
+    System.Console.WriteLine("Ср");
+}
+else if (x == 4)
+{
+    System.Console.WriteLine("Чт");
+}
+else if (x == 5)
+{
+    System.Console.WriteLine("Пт");
+}
+else if (x == 6)
+{
+    System.Console.WriteLine("Сб");
+}
+else if (x == 5)
+{
+    System.Console.WriteLine("Вс");
+}
+else
+{
+    System.Console.WriteLine("Не корректное значения");
+}
