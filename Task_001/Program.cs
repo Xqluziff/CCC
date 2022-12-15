44ffb86c9868ef07171354ac282921063a320ceb
XQluziff <xqluziff@yandex.ru>
2022-12-15 21:38:59 +0300

    Task_00 & Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*1. Напишите программу, которая на вход принимает два
+числа и проверяет, является ли первое число квадратом
+второго.
+a = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+a = 9, b = -3 -> да
+a = -3 b = 9 -> нет
+3. Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+
+System.Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y * y)
+{
+
+    int res = x / y;
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
+
+
+
+
