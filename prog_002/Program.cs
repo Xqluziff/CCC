62dfe34890ab5f1d4e9f03b84d04e10e997ee549
XQluziff <xqluziff@yandex.ru>
2022-12-15 15:48:29 +0300

    commit 3

--- prog_002/Program.cs
+++ prog_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+/*Console.SetCursorPosition(10,10);
+Console.WriteLine("+");*/
+
+int Xa = 75, Ya = 1,
+    Xb = 1, Yb = 50,
+    Xc = 150, Yc = 50;
+Console.SetCursorPosition(Xa,Ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(Xb,Yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(Xc,Yc);
+Console.WriteLine("+");
+ 
+
+
