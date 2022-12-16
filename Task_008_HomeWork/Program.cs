b6bcd246a6df226d141d9d16f7fcee7d13a76d90
Xqluziff <xqluziff@yandex.ru>
2022-12-16 14:50:20 +0300

    HomeWork_008

---
+++ Task_008_HomeWork/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+System.Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count <= value)
+{
+    System.Console.WriteLine(count+=2);
+}
