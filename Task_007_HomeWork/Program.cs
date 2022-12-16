50291709bbef9f7c87161cda5a7d3c9a06027ec0
Xqluziff <xqluziff@yandex.ru>
2022-12-16 11:40:49 +0300

    HomeWork_007 OK

---
+++ Task_007_HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿/*: Напишите программу, которая на вход принимает число
+ и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+System.Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if(value%2==0)System.Console.WriteLine($"Число {value} четное");
+else System.Console.WriteLine($"Число {value} не чётное");
