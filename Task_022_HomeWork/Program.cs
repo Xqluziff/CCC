1b44913ba5e5e7c599221af3f2bffbe720fb9fd8
Xqluziff <xqluziff@yandex.ru>
2022-12-27 13:33:27 +0300

    HomeWork 022

---
+++ Task_022_HomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает
+ на вход число (N) и выдаёт
+  таблицу кубов чисел от 1 до N.
+
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125*/
+
+System.Console.WriteLine("Enter digit");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (number > 0) TableSquer(number);
+else System.Console.WriteLine("Не корректые данные");
+
+void TableSquer(double num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        //System.Console.WriteLine($"{count}\t"+Math.Pow(count,2));
+        System.Console.WriteLine($"{count,1} {Math.Pow(count, 3),10}");
+        count++;
+    }
+}
